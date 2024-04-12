1e603d42e63215e722e11693db8b200cad2649b0
Riley Meyerkorth <rileymeyerkorth@gmail.com>
2024-04-11 22:10:14 -0500

    clean up code and add comments

--- HiFive/src/comm.c
+++ HiFive/src/comm.c
@@ -23,18 +23,13 @@ void auto_brake(int devid)
    uint8_t dist_L   = 0;
    uint8_t dist_H   = 0;
    uint16_t dist    = 0;
-   int gpio         = RED_LED;
 
    // Check first 2 bytes to ensure frame headers exist and are valid
    if ('Y' == ser_read(devid) && 'Y' == ser_read(devid)) {
-        // Read dist L byte
-        dist_L = ser_read(devid);
-        // Read dist_H byte
-        dist_H = ser_read(devid);
 
-        // Build the distance value
-        dist = (dist_H << 8) | dist_L;
-
+        dist_L = ser_read(devid); // Read dist L byte
+        dist_H = ser_read(devid); // Read dist_H byte
+        dist = (dist_H << 8) | dist_L; // Build the distance value by shifting the bits
 
         //=== READING DISTANCE
         if (dist > 200) {
@@ -56,9 +51,9 @@ void auto_brake(int devid)
             //== Must stop - flash red and turn other colors off
             gpio_write(GREEN_LED, OFF);
             gpio_write(RED_LED, ON);
-            delay(100);
+            delay(100); // Delay 100ms
             gpio_write(RED_LED, OFF);
-            delay(100);
+            delay(100); // Delay 100ms
         }
 
         // OPTIONAL - Print the distance
@@ -79,46 +74,34 @@ void steering(int gpio, int pos)
     // Task-3: 
     // Your code goes here (Use Lab 05 for reference)
     // Check the project document to understand the task
-
-    // Basically, you need to take the input angle "pos" and generate the
-    // To generate the pwm signals, use gpio_write() and delay_usec()
     
-
     // Define the servo pulses
-    int SERVO_PULSE_MAX = 2400; // 2400 us
-    int SERVO_PULSE_MIN = 544; // 544 us
-    int SERVO_PERIOD = 20000; // 20000 us (or 20ms)
+    int SERVO_PULSE_MAX = 2400;     // 2400 us
+    int SERVO_PULSE_MIN = 544;      // 544 us
+    int SERVO_PERIOD    = 20000;    // 20000 us (or 20ms)
 
-
-    // Gets the pulse rate in pulses per nanosecond
-    int pulse_rate = (SERVO_PULSE_MAX - SERVO_PULSE_MIN) / 180;
-    // Get the current position's pulse rate
-    int position_rate = (pulse_rate) * pos;
-    // Must account for the SERVO_PULSE_MIN that was subtracted initially. The total duty delay.
-    int duty_delay = position_rate + SERVO_PULSE_MIN;
-    // Turn the GPIO pin on
-    gpio_write(gpio, ON);
-    // Delay by the duty amount
-    delay_usec(duty_delay);
-    // Turn the GPIO pin off
-    gpio_write(gpio, OFF);
-    // Delay for the servo period (1 minute = 1000000 microseconds, 1000000 microseconds / 50 loops = 20000 microseconds, or 20 milliseconds)
-    delay_usec(SERVO_PERIOD);
+    int pulse_rate = (SERVO_PULSE_MAX - SERVO_PULSE_MIN) / 180; // Gets the pulse rate in pulses per nanosecond
+    int position_rate = (pulse_rate) * pos;                     // Get the current position's pulse rate
+    int duty_delay = position_rate + SERVO_PULSE_MIN;           // Must account for the SERVO_PULSE_MIN that was subtracted initially. The total duty delay.
+    gpio_write(gpio, ON);                                       // Turn the GPIO pin on
+    delay_usec(duty_delay);                                     // Delay by the duty amount
+    gpio_write(gpio, OFF);                                      // Turn the GPIO pin off
+    delay_usec(SERVO_PERIOD);                                   // Delay for the servo period (1 minute = 1000000 microseconds, 1000000 microseconds / 50 loops = 20000 microseconds, or 20 milliseconds)
 }
 
 
 int main()
 {
-    // initialize UART channels
-    ser_setup(0); // uart0
-    ser_setup(1); // uart1
-    int pi_to_hifive = 1; //The connection with Pi uses uart 1
-    int lidar_to_hifive = 0; //the lidar uses uart 0
+    // Initialize UART channels
+    ser_setup(0); // UART0
+    ser_setup(1); // UART1
+    int pi_to_hifive    = 1; // The connection with Pi uses uart 1
+    int lidar_to_hifive = 0; // The lidar uses uart 0
     
     printf("\nUsing UART %d for Pi -> HiFive", pi_to_hifive);
     printf("\nUsing UART %d for Lidar -> HiFive", lidar_to_hifive);
     
-    //Initializing PINs
+    // Initializing PINs
     gpio_mode(PIN_19, OUTPUT);
     gpio_mode(RED_LED, OUTPUT);
     gpio_mode(BLUE_LED, OUTPUT);
@@ -129,10 +112,10 @@ int main()
 
     while (1) {
 
-        auto_brake(lidar_to_hifive); // measuring distance using lidar and braking
-        int angle = read_from_pi(pi_to_hifive); //getting turn direction from pi
-        printf("\nangle=%d", angle) 
-        int gpio = PIN_19; 
+        auto_brake(lidar_to_hifive);            // Measuring distance using lidar and braking
+        int angle = read_from_pi(pi_to_hifive); // Getting turn direction from pi
+        printf("\nangle=%d", angle)             // Print the angle of the servo
+        int gpio = PIN_19;                      // Set the GPIO pin to PIN_19
         for (int i = 0; i < 10; i++){
             // Here, we set the angle to 180 if the prediction from the DNN is a positive angle
             // and 0 if the prediction is a negative angle.
