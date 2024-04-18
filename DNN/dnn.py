398edbd4e4baf6f4e8cb090a8e7f23098b67c9fe
soulsoiledit <aryankevat@proton.me>
2024-04-18 10:00:52 -0500

    add write function to dnn.py

--- DNN/dnn.py
+++ DNN/dnn.py
@@ -91,8 +91,7 @@ while(1):
 		# prediction from the model. This is so that
 		# the HiFive can run the other functions in between
 		if count%4 == 0:
-			pass
-			#Your code here.
+			ser1.write(deg.astype(np.int16).to_bytes())
 
 		
         
